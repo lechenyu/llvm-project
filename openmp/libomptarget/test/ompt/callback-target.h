e260d9a3e168d3f6fdaa8793ad770c39ec3b70d1
Lechen Yu <lechenyu21@gmail.com>
2021-07-06 21:24:04 -0400

    [ompt] Add a test callback for device_mem event

--- openmp/libomptarget/test/ompt/callback-target.h
+++ openmp/libomptarget/test/ompt/callback-target.h
@@ -112,4 +112,265 @@ static void format_mapping_flag(int flag, char *buffer) {
 #undef ADD_FS
 }
 
+static void format_device_mem_flag(int flag, char *buffer) {
+  char *progress = buffer;
+
+#define ADD_FS                                     \
+  do {                                             \
+    if (progress != buffer) {                      \
+      progress += sprintf(progress, "|");          \
+    }                                              \
+  } while(0)
+
+  if (flag & ompt_device_mem_flag_to) {
+    progress += sprintf(progress, "ompt_device_mem_flag_to");
+  }
+  if (flag & ompt_device_mem_flag_from) {
+    ADD_FS;
+    progress += sprintf(progress, "ompt_device_mem_flag_from");
+  }
+  if (flag & ompt_device_mem_flag_alloc) {
+    ADD_FS;
+    progress += sprintf(progress, "ompt_device_mem_flag_alloc");
+  }
+  if (flag & ompt_device_mem_flag_release) {
+    ADD_FS;
+    progress += sprintf(progress, "ompt_device_mem_flag_release");
+  }
+  if (flag & ompt_device_mem_flag_associate) {
+    ADD_FS;
+    progress += sprintf(progress, "ompt_device_mem_flag_associate");
+  }
+  if (flag & ompt_device_mem_flag_disassociate) {
+    ADD_FS;
+    progress += sprintf(progress, "ompt_device_mem_flag_disassociate");
+  }
+
+#undef ADD_FS
+}
+
+static void on_ompt_callback_target_emi(
+        ompt_target_t kind,
+        ompt_scope_endpoint_t endpoint,
+        int device_num,
+        ompt_data_t *task_data,
+        ompt_data_t *target_task_data,
+        ompt_data_t *target_data,
+        const void *codeptr_ra)
+{
+  switch(endpoint)
+  {
+    case ompt_scope_begin:
+      if (target_data->ptr)
+      {
+        printf("%s\n", "0: target_data initially not null");
+      }
+      target_data->value = ompt_get_unique_id();
+
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_emi_begin: task_id=%" PRIu64
+      ", target_task_id=%" PRIu64 ", target_id=%" PRIu64 ", device_num=%" PRIu32
+      ", kind=%s" ", codeptr_ra=%p" "\n", ompt_get_thread_data()->value, task_data->value,
+              target_task_data ? target_task_data->value : 0, target_data->value, device_num,
+              ompt_target_t_values[kind], codeptr_ra);
+      break;
+    case ompt_scope_end:
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_emi_end: task_id=%" PRIu64
+      ", target_task_id=%" PRIu64 ", target_id=%" PRIu64 ", device_num=%" PRIu32
+      ", kind=%s" ", codeptr_ra=%p" "\n", ompt_get_thread_data()->value, task_data->value,
+              target_task_data ? target_task_data->value : 0, target_data->value, device_num,
+              ompt_target_t_values[kind], codeptr_ra);
+      break;
+    case ompt_scope_beginend:
+      printf("ompt_scope_beginend should never be passed to %s\n", __func__);
+      exit(-1);
+  }
+}
+
+static void on_ompt_callback_target_data_op_emi(
+        ompt_scope_endpoint_t endpoint,
+        ompt_data_t *target_task_data,
+        ompt_data_t *target_data,
+        ompt_id_t *host_op_id,
+        ompt_target_data_op_t optype,
+        void *src_addr,
+        int src_device_num,
+        void *dest_addr,
+        int dest_device_num,
+        size_t bytes,
+        const void *codeptr_ra)
+{
+  switch (endpoint) {
+    case ompt_scope_begin:
+      *host_op_id = ompt_get_unique_id();
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_data_op_emi_begin: target_task_id=%" PRIu64
+      ", target_id=%" PRIu64 ", host_op_id=%" PRIu64 ", optype=%s, src_addr=%p, src_device_num=%" PRIu32
+      ", dest_addr=%p, dest_device_num=%" PRIu32 ", bytes=%" PRIu64 ", codeptr_ra=%p" "\n",
+          ompt_get_thread_data()->value, target_task_data ? target_task_data->value : 0, target_data->value, *host_op_id,
+          ompt_target_data_op_t_values[optype], src_addr, src_device_num, dest_addr, dest_device_num, bytes, codeptr_ra);
+      break;
+    case ompt_scope_end:
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_data_op_emi_end: target_task_id=%" PRIu64
+      ", target_id=%" PRIu64 ", host_op_id=%" PRIu64 ", optype=%s, src_addr=%p, src_device_num=%" PRIu32
+      ", dest_addr=%p, dest_device_num=%" PRIu32 ", bytes=%" PRIu64 ", codeptr_ra=%p" "\n",
+          ompt_get_thread_data()->value, target_task_data ? target_task_data->value : 0, target_data->value, *host_op_id,
+          ompt_target_data_op_t_values[optype], src_addr, src_device_num, dest_addr, dest_device_num, bytes, codeptr_ra);
+      break;
+    case ompt_scope_beginend:
+      if (optype == ompt_target_data_associate || optype == ompt_target_data_disassociate) {
+        *host_op_id = ompt_get_unique_id();
+        printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_data_op_emi_beginend: target_task_id=%" PRIu64
+        ", target_id=%" PRIu64 ", host_op_id=%" PRIu64 ", optype=%s, src_addr=%p, src_device_num=%" PRIu32
+        ", dest_addr=%p, dest_device_num=%" PRIu32 ", bytes=%" PRIu64 ", codeptr_ra=%p" "\n",
+                ompt_get_thread_data()->value, target_task_data ? target_task_data->value : 0, target_data->value, *host_op_id,
+                ompt_target_data_op_t_values[optype], src_addr, src_device_num, dest_addr, dest_device_num, bytes, codeptr_ra);
+      } else {
+        printf("ompt_scope_beginend should never be passed to %s\n", __func__);
+        exit(-1);
+      }
+      break;
+  }
+}
+
+static void on_ompt_callback_target_map_emi(
+        ompt_data_t *target_data,
+        unsigned int nitems,
+        void **host_addr,
+        void **device_addr,
+        size_t *bytes,
+        unsigned int *mapping_flags,
+        const void *codeptr_ra)
+{
+  char buffer[2048];
+  printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_map_emi: target_id=%" PRIu64
+  ", nitems=%" PRIu32 ", codeptr_ra=%p" "\n", ompt_get_thread_data()->value, target_data->value,
+          nitems, codeptr_ra);
+  for (int i = 0; i < nitems; i++)
+  {
+    format_mapping_flag(mapping_flags[i], buffer);
+    printf("%" PRIu64 ":" _TOOL_PREFIX " map: host_addr=%p, device_addr=%p, bytes=%" PRIu64 ", mapping_flag=%s" "\n",
+            ompt_get_thread_data()->value, host_addr[i], device_addr[i], bytes[i], buffer);
+    buffer[0] = '\0'; // reset the buffer
+  }
+}
+
+static void on_ompt_callback_target_submit_emi(
+        ompt_scope_endpoint_t endpoint,
+        ompt_data_t *target_data,
+        ompt_id_t *host_op_id,
+        unsigned int requested_num_teams)
+{
+  switch(endpoint) {
+    case ompt_scope_begin:
+      *host_op_id = ompt_get_unique_id();
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_submit_emi_begin: target_id=%" PRIu64
+      ", host_op_id=%" PRIu64 ", requested_num_teams=%" PRIu32 "\n", ompt_get_thread_data()->value, target_data->value, *host_op_id,
+          requested_num_teams);
+      break;
+    case ompt_scope_end:
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_submit_emi_end: target_id=%" PRIu64
+      ", host_op_id=%" PRIu64 ", requested_num_teams=%" PRIu32 "\n", ompt_get_thread_data()->value, target_data->value, *host_op_id,
+          requested_num_teams);
+      break;
+    case ompt_scope_beginend:
+      printf("ompt_scope_beginend should never be passed to %s\n", __func__);
+      exit(-1);
+  }
+}
+
+static void on_ompt_callback_target(
+        ompt_target_t kind,
+        ompt_scope_endpoint_t endpoint,
+        int device_num,
+        ompt_data_t *task_data,
+        ompt_id_t target_id,
+        const void *codeptr_ra)
+{
+  switch(endpoint)
+  {
+    case ompt_scope_begin:
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_begin: task_id=%" PRIu64
+      ", target_id=%" PRIu64 ", device_num=%" PRIu32 ", kind=%s" ", codeptr_ra=%p" "\n",
+          ompt_get_thread_data()->value, task_data->value, target_id, device_num,
+          ompt_target_t_values[kind], codeptr_ra);
+      break;
+    case ompt_scope_end:
+      printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_end: task_id=%" PRIu64
+      ", target_id=%" PRIu64 ", device_num=%" PRIu32 ", kind=%s" ", codeptr_ra=%p" "\n",
+          ompt_get_thread_data()->value, task_data->value, target_id, device_num,
+          ompt_target_t_values[kind], codeptr_ra);
+      break;
+    case ompt_scope_beginend:
+      printf("ompt_scope_beginend should never be passed to %s\n", __func__);
+      exit(-1);
+  }
+}
+
+static void on_ompt_callback_target_data_op(
+        ompt_id_t target_id,
+        ompt_id_t host_op_id,
+        ompt_target_data_op_t optype,
+        void *src_addr,
+        int src_device_num,
+        void *dest_addr,
+        int dest_device_num,
+        size_t bytes,
+        const void *codeptr_ra)
+{
+  printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_data_op: target_id=%" PRIu64 ", host_op_id=%" PRIu64
+  ", optype=%s, src_addr=%p, src_device_num=%" PRIu32 ", dest_addr=%p, dest_device_num=%" PRIu32
+  ", bytes=%" PRIu64 ", codeptr_ra=%p" "\n", ompt_get_thread_data()->value, target_id, host_op_id,
+          ompt_target_data_op_t_values[optype], src_addr, src_device_num, dest_addr, dest_device_num, bytes, codeptr_ra);
+}
+
+static void on_ompt_callback_target_map(
+        ompt_id_t target_id,
+        unsigned int nitems,
+        void **host_addr,
+        void **device_addr,
+        size_t *bytes,
+        unsigned int *mapping_flags,
+        const void *codeptr_ra)
+{
+  char buffer[2048];
+  printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_map: target_id=%" PRIu64
+  ", nitems=%" PRIu32 ", codeptr_ra=%p" "\n", ompt_get_thread_data()->value, target_id,
+          nitems, codeptr_ra);
+  for (int i = 0; i < nitems; i++)
+  {
+    format_mapping_flag(mapping_flags[i], buffer);
+    printf("%" PRIu64 ":" _TOOL_PREFIX " map: host_addr=%p, device_addr=%p, bytes=%" PRIu64 ", mapping_flag=%s" "\n",
+            ompt_get_thread_data()->value, host_addr[i], device_addr[i], bytes[i], buffer);
+    buffer[0] = '\0'; // reset the buffer
+  }
+}
+
+static void on_ompt_callback_target_submit(
+        ompt_id_t target_id,
+        ompt_id_t host_op_id,
+        unsigned int requested_num_teams)
+{
+  printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_target_submit: target_id=%" PRIu64
+  ", host_op_id=%" PRIu64 ", requested_num_teams=%" PRIu32 "\n", ompt_get_thread_data()->value, target_id, host_op_id,
+          requested_num_teams);
+}
+
+static void on_ompt_callback_device_mem(
+        ompt_data_t *target_task_data,
+        ompt_data_t *target_data,
+        unsigned int device_mem_flag,
+        void *orig_base_addr,
+        void *orig_addr,
+        int orig_device_num,
+        void *dest_addr,
+        int dest_device_num,
+        size_t bytes,
+        const void *codeptr_ra)
+{
+  char buffer[2048];
+  format_device_mem_flag(device_mem_flag, buffer);
+  printf("%" PRIu64 ":" _TOOL_PREFIX " ompt_event_device_mem: target_task_id=%" PRIu64 ", target_id=%" PRIu64
+  ", device_mem_flag=%s, orig_base_addr=%p, orig_addr=%p, orig_device_num=%" PRIu32 ", dest_addr=%p, dest_device_num=%" PRIu32
+  ", bytes=%" PRIu64 ", codeptr_ra=%p" "\n", ompt_get_thread_data()->value, target_task_data->value, target_data->value,
+  buffer, orig_base_addr, orig_addr, orig_device_num, dest_addr, dest_device_num, bytes, codeptr_ra);
+}
 #endif //__CALLBACK_TARGET_H__
